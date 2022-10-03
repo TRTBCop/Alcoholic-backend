ce8547d926c9aa5b31e05b46f9d3470417742d60
seunghunHan1 <hkgb0009@gmail.com>
2022-10-03 18:47:38 +0900

    [AH] 알코올 히스토리 생성 api 스웨거 작성

--- src/controllers/alcHistoryCtrl.ts
+++ src/controllers/alcHistoryCtrl.ts
@@ -1,39 +1,101 @@
-import e from 'express';
-import { createAlcHistory, fetchAlcHistory } from '../services/alcHistoryService';
-import { checkToken } from '../services/authService';
-import { ResultCode } from '../types/resultCode';
+import { AlcHistoryFormData } from "../models/alcHistory";
+import {
+  createAlcHistory,
+  fetchAlcHistory,
+} from "../services/alcHistoryService";
+import { checkToken } from "../services/authService";
+import { ResultCode } from "../types/resultCode";
 
 const alcHistoryCtrl = {
-    async getAlcHistory(req: any, res: any) {
-        const userInfo = await checkToken(req, res);
-        if (userInfo?.uid === undefined) {
-            res.status(ResultCode.Unauthorized).json({ code: ResultCode.Unauthorized, message: '로그인이 되어있지 않습니다.', data: null });
-        } else {
-            const data = await fetchAlcHistory(userInfo.uid);
-            res.json({ code: ResultCode.Success, message: 'getAlcHistory List Data', data: data });
+  async getAlcHistory(req: any, res: any) {
+    const userInfo = await checkToken(req, res);
+    if (userInfo?.uid === undefined) {
+      res.status(ResultCode.Unauthorized).json({
+        code: ResultCode.Unauthorized,
+        message: "로그인이 되어있지 않습니다.",
+        data: null,
+      });
+    } else {
+      const data = await fetchAlcHistory(userInfo.uid);
+      res.json({
+        code: ResultCode.Success,
+        message: "getAlcHistory List Data",
+        data: data,
+      });
+    }
+  },
+  async getAlcHistoryDetail(req: any, res: any) {
+    const data = null;
+    res.json({
+      code: ResultCode.Success,
+      message: "getAlcHistoryDetail",
+      data: data,
+    });
+  },
+  async updateAlcHistory(req: any, res: any) {
+    const data = null;
+    res.json({
+      code: ResultCode.Success,
+      message: "updateAlcHistory",
+      data: data,
+    });
+  },
+  async addAlcHistory(req: any, res: any) {
+    const userInfo = await checkToken(req, res);
+    if (!userInfo) {
+      res.status(ResultCode.Unauthorized).json({
+        code: ResultCode.Unauthorized,
+        message: "로그인이 되어있지 않습니다.",
+        data: null,
+      });
+    } else {
+      const reqData: AlcHistoryFormData = req.body.data;
+      if (
+        !reqData?.memo ||
+        !reqData?.writeDateDay ||
+        !reqData?.writeDateMonth ||
+        !reqData?.writeDateYear
+      ) {
+        res.json({
+          code: ResultCode.BadRequest,
+          message: "파라미터에 값이 올바르지 않습니다.",
+          data: null,
+        });
+      } else {
+        try {
+          const result = await createAlcHistory(userInfo.uid, req.body.data);
+          if (result) {
+            res.json({
+              code: ResultCode.Success,
+              message: "정상적으로 성공하였습니다.",
+              data: result,
+            });
+          } else {
+            res.json({
+              code: ResultCode.BadRequest,
+              message: "파이어베이스 오류",
+              data: result,
+            });
+          }
+        } catch (err) {
+          console.log(err);
+          res.json({
+            code: ResultCode.BadRequest,
+            message: "파이어베이스 오류",
+            data: null,
+          });
         }
-    },
-    async getAlcHistoryDetail(req: any, res: any) {
-        const data = null;
-        res.json({ code: ResultCode.Success, message: 'getAlcHistoryDetail', data: data });
-    },
-    async updateAlcHistory(req: any, res: any) {
-        const data = null;
-        res.json({ code: ResultCode.Success, message: 'updateAlcHistory', data: data });
-    },
-    async addAlcHistory(req: any, res: any) {
-        const userInfo = await checkToken(req, res);
-        if (!userInfo) {
-            res.status(ResultCode.Unauthorized).json({ code: ResultCode.Unauthorized, message: '로그인이 되어있지 않습니다.', data: null });
-        } else {
-            const data = await createAlcHistory(userInfo.uid, req.body.data);
-            res.json({ code: ResultCode.Success, message: 'addAlcHistory', data: data });
-        }
-    },
-    async deleteAlcHistory(req: any, res: any) {
-        const data = null;
-        res.json({ code: ResultCode.Success, message: 'deleteAlcHistory', data: data });
-    },
-}
+      }
+    }
+  },
+  async deleteAlcHistory(req: any, res: any) {
+    const data = null;
+    res.json({
+      code: ResultCode.Success,
+      message: "deleteAlcHistory",
+      data: data,
+    });
+  },
+};
 
-export { alcHistoryCtrl }
+export { alcHistoryCtrl };
